9e65b4d40a6dc0f59aab7fd75ba4de00f7d09a29
AK08 <alenkottaram2000@gmail.com>
2023-08-21 12:05:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=b059fc30';
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=b059fc30';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
